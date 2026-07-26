7a97a4bc757644f868b340d4430c1f931516d880
oindrila0101 <oindrilapramanik18@gmail.com>
2026-07-26 21:27:45 +0530

    static

--- src/DSA_qs/oops/BoxWeight.java
+++ src/DSA_qs/oops/BoxWeight.java
@@ -7,9 +7,23 @@ public class BoxWeight extends Box{
         this.weight=-1;
     }
 
+    BoxWeight(BoxWeight other){
+        super(other);
+        weight=other.weight;
+    }
+
+    BoxWeight(double side, double weight) {
+        super(side);
+        this.weight = weight;
+    }
+
     public BoxWeight(double l,double h,double w,double weight){
-        super(l,h,w); // call the parent class constructor
+        super(l,h,w);
+        //super() is used in a child class to call the constructor of the parent class.
+        // call the parent class constructor
         //used to initialize values present in parent class
+
+        //System.out.println(super.w);
         this.weight=weight;
     }
 
